1ef86acd8918ae8c545ff766df20f31c508df6fe
Andrey Morskoy <amorskoy@codeminders.com>
2015-04-20 16:06:42 +0300

    Commin solution for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching inversion of invertible (as assumed) matrix
+## Test case:
+## a <- matrix(c(3,1,2,1),nrow=2,ncol=2)
+## ac = makeCacheMatrix(a)
+## sac = cacheSolve(ac)
+## sac = cacheSolve(ac)
+## You should see: 
+## 1. On both calls of cacheSolve - inverted result = matrix(c(1,-1,-2,3), 2, 2) 
+## 2. On the second call of cacheSolve - message "getting cached data"
+## 3. Result of round(ac$get() %*% sac) equal to matrix(c(1,0,0,1),2,2)
 
-## Write a short comment describing this function
+## Prepare version of matrix supporing cached inversion result
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinvert <- function(invert) i <<- invert
+  getinvert <- function() i
+  list(set = set, get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Return cached inverted value for invertable matrix x.
+## If value is not cached - then it calculates inversion first 
+## and returns the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getinvert()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinvert(i)
+  
+  return(i)
 }
